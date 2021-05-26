1c6d4e69cd9ac0674803b756cc484ed478e6ad18
v0dLeX <vodlex@hotmail.com>
2021-05-27 04:48:04 +0500

    27/05/2021 v2

--- classes/Cow.php
+++ classes/Cow.php
@@ -3,11 +3,6 @@
 
 class Cow extends Animal
 {
-    public $id;
-
-    public function __construct(){
-        $this->id = substr(md5(rand()), 0,6);
-    }
 
     public function getProduction(): int
     {
